751d2df1c123eb02ae968f19f35ade5cb72bf1f6
mohamed_hamdi_mahmoud <01124833532mo>
2024-10-02 20:56:52 +0300

    Part05 ASP.NEW Core Web Apis - Project Structure

---
+++ LinkDev.Talabat.Apis/Program.cs
@@ -0,0 +1,39 @@
+
+namespace LinkDev.Talabat.Apis
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region Configure Services
+            var WebApplicationBuilder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            WebApplicationBuilder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            WebApplicationBuilder.Services.AddEndpointsApiExplorer();
+            WebApplicationBuilder.Services.AddSwaggerGen();
+
+            #endregion
+            var app = WebApplicationBuilder.Build();
+
+            #region Configure Kestrel Middlewares
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+
+
+            app.MapControllers();
+
+            #endregion
+            app.Run();
+        }
+    }
+}
