b38bb312ce070a4a8cbafa2ee1983002ea82f589
mohamed_hamdi_mahmoud <01124833532mo>
2024-10-08 18:57:57 +0300

    Part03 Refactor BaseAuditableEntityConfigurations

--- LinkDev.Talabat.Infrastructure.Persistence/Data/Config/Base/BaseEntityConfigurations.cs
+++ LinkDev.Talabat.Infrastructure.Persistence/Data/Config/Base/BaseEntityConfigurations.cs
@@ -9,20 +9,12 @@ using System.Threading.Tasks;
 namespace LinkDev.Talabat.Infrastructure.Persistence.Data.Config.Base
 {
     internal class BaseEntityConfigurations<TEntity, TKey> : IEntityTypeConfiguration<TEntity>
-        where TEntity : BaseAuditableEntity<TKey> where TKey : IEquatable<TKey>
+        where TEntity : BaseEntity<TKey> where TKey : IEquatable<TKey>
     {
         public virtual void Configure(EntityTypeBuilder<TEntity> builder)
         {
-            builder.Property(E => E.Id).ValueGeneratedOnAdd();
+            builder.Property(e => e.Id).ValueGeneratedOnAdd();
 
-            builder.Property(E => E.CreatedBy).IsRequired();
-
-            builder.Property(E => E.CreatedOn).IsRequired(); 
-
-
-            builder.Property(E => E.LastModifiedBy).IsRequired();
-
-            builder.Property(E => E.LastModifiedOn).IsRequired();
         }
     }
 }
