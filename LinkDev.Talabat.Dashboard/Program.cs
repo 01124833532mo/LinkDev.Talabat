e20ccd51091bc40258fe85ed0ebba869cc48cb7e
mohamed_hamdi_mahmoud <01124833532mo>
2024-10-25 23:30:40 +0300

    Part01 Frontend Structure

--- LinkDev.Talabat.Dashboard/Program.cs
+++ LinkDev.Talabat.Dashboard/Program.cs
@@ -1,3 +1,9 @@
+using LinkDev.Talabat.Core.Domain.Entities.Identity;
+using LinkDev.Talabat.Infrastructure.Persistence.Data;
+using LinkDev.Talabat.Infrastructure.Persistence.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace LinkDev.Talabat.Dashboard
 {
     public class Program
@@ -8,6 +14,42 @@ namespace LinkDev.Talabat.Dashboard
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+           builder. Services.AddDbContext<StoreDbContext>((optionsBuilder) =>
+            {
+
+                optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("StoreContext"));
+            });
+
+
+           builder. Services.AddDbContext<StoreIdentityDbContext>((optionsBuilder) =>
+            {
+
+                optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("IdentityContext"));
+            });
+
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(identityoptions =>
+            {
+
+                identityoptions.User.RequireUniqueEmail = true;
+
+                identityoptions.SignIn.RequireConfirmedPhoneNumber = true;
+                identityoptions.SignIn.RequireConfirmedEmail = true;
+                //identityoptions.SignIn.RequireConfirmedAccount = true;
+
+                //identityoptions.Password.RequireNonAlphanumeric = true;
+                //identityoptions.Password.RequiredUniqueChars = 2;
+                //identityoptions.Password.RequiredLength = 6;
+                //identityoptions.Password.RequireDigit = true;
+                //identityoptions.Password.RequireLowercase = true;
+                //identityoptions.Password.RequireUppercase = true;
+
+                identityoptions.Lockout.AllowedForNewUsers = true;
+                identityoptions.Lockout.MaxFailedAccessAttempts = 10;
+                identityoptions.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+
+
+            }).AddEntityFrameworkStores<StoreIdentityDbContext>();
 
             var app = builder.Build();
 
