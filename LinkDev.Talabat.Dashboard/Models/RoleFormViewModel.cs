a42a78412e681edc91a166412bfb990841cffe60
mohamed_hamdi_mahmoud <01124833532mo>
2024-10-26 00:41:40 +0300

    Part02 Roles

---
+++ LinkDev.Talabat.Dashboard/Models/RoleFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkDev.Talabat.Dashboard.Models
+{
+	public class RoleFormViewModel
+	{
+		[Required(ErrorMessage ="name is requered")]
+		[StringLength(256)]
+
+        public string Name { get; set; }
+    }
+}
