8df71fb5947e92c16c203eb38f153ceb46fce923
mohamed_hamdi_mahmoud <01124833532mo>
2024-12-06 18:50:18 +0200

    Part 06 implement cache service

---
+++ LinkDev.Talabat.Core.Application.Abstraction/Common/Contract/Infrastructure/IResponseCacheService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Application.Abstraction.Common.Contract.Infrastructure
+{
+	public interface IResponseCacheService
+	{
+		Task CacheResponseAsynce(string key, object respons,TimeSpan TimeToLive );
+
+		Task<string?> GetCachedResponsAsync(string key);
+
+	}
+}
